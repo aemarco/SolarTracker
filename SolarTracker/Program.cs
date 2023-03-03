20687d9dabf3333bcf79f2b6e43ece12a080b7cd
aemarco <aemarco@outlook.de>
2023-03-03 19:13:03 +0100

    converted to api project

--- SolarTracker/Program.cs
+++ SolarTracker/Program.cs
@@ -1,23 +1,51 @@
-﻿using SolarTracker.Configuration;
-using SolarTracker.Services;
-using System;
-using System.Globalization;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 
-CultureInfo.DefaultThreadCurrentCulture = new CultureInfo(CultureInfo.CurrentCulture.Name)
+//CultureInfo.DefaultThreadCurrentCulture = new CultureInfo(CultureInfo.CurrentCulture.Name)
+//{
+//    NumberFormat = new NumberFormatInfo()
+//};
+//var appSettings = new AppSettings
+//{
+//    ApiKey = string.Empty,
+//    Latitude = 0,
+//    Longitude = 0
+//};
+
+//var s = new AstroApiService(appSettings);
+//var si = await s.GetSunInfo();
+
+
+//Console.WriteLine(si);
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    NumberFormat = new NumberFormatInfo()
-};
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
-var appSettings = new AppSettings
-{
-    ApiKey = string.Empty,
-    Latitude = 0,
-    Longitude = 0
-};
+app.UseAuthorization();
 
-var s = new AstroApiService(appSettings);
-var si = await s.GetSunInfo();
+app.MapControllers();
+
+app.Run();
 
 
-Console.WriteLine(si);
+
+
